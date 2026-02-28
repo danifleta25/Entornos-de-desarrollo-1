73ee8d5b2b0aa0fe48c25df613ce5fd7d109425f
Dani Fleta <danifleta25@gmail.com>
2026-02-28 17:21:44 +0100

    Password source code

---
+++ src/main/java/ud4/methods/PasswordStrengthChecker.java
@@ -0,0 +1,19 @@
+package ud4.methods;
+
+
+
+
+public class PasswordStrengthChecker {
+
+    public enum PasswordStrength{
+        INVALID,
+        WEAK,
+        MEDIUM,
+        STRONG
+    }
+
+
+    public static void isPasswordStrong(String password){
+
+    }
+}

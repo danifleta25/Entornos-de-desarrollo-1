a302b463736654544075ce2f9733f254da262852
Dani Fleta <danifleta25@gmail.com>
2026-02-28 17:43:41 +0100

    Source code Method completed

--- src/main/java/ud4/methods/PasswordStrengthChecker.java
+++ src/main/java/ud4/methods/PasswordStrengthChecker.java
@@ -13,7 +13,32 @@ public class PasswordStrengthChecker {
     }
 
 
-    public static void isPasswordStrong(String password){
+    public static PasswordStrength isPasswordStrong(String password){
+        PasswordStrength fortaleza = null;
+        int contadorFortaleza = 0;
 
+        if(password == null || password.length()<8){
+            fortaleza = PasswordStrength.INVALID;
+            return fortaleza;
+        }
+
+        if(password.contains("[a-z]")){
+            contadorFortaleza += 1;
+        }
+        if(password.contains("[A-Z]")){
+            contadorFortaleza += 1;
+        }
+        if(password.contains("[0-9]")){
+            contadorFortaleza += 1;
+        }
+
+
+        switch (contadorFortaleza){
+            case 1 -> fortaleza = PasswordStrength.WEAK;
+            case 2 -> fortaleza = PasswordStrength.MEDIUM;
+            case 3 -> fortaleza = PasswordStrength.STRONG;
+        }
+
+        return fortaleza;
     }
 }

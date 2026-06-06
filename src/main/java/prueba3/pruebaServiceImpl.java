9ee4fbc8278aa61b22c86e4267f37b6f27b52324
DANI <dani@gmail.com>
2026-06-06 17:19:28 +0200

    SC prueba añadido

---
+++ src/main/java/prueba3/pruebaServiceImpl.java
@@ -0,0 +1,19 @@
+package prueba3;
+
+public class pruebaServiceImpl {
+
+
+    int a;
+    int b;
+
+    public int suma(){
+        return a + b;
+    }
+
+    public int resta(){
+        return a - b;
+    }
+
+    public int div(){return a/b;}
+
+}

272a949ed2d0b93a5565f20f31ae399d3695fd6a
DANI <dani@gmail.com>
2026-06-07 20:49:17 +0200

    Test agregarLibro() integrado en develop

--- src/test/java/pruebaBiblioteca/BibliotecaServiceTest.java
+++ src/test/java/pruebaBiblioteca/BibliotecaServiceTest.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class BibliotecaServiceTest {
 
     /*
+    Test creado
+    para el metodo
+    buscarLibro()
+    */
+    /*
     Este es el test
     de agregarLibro()
 
      */
-
 
 }
